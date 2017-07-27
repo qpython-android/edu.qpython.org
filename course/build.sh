3ff7979cd48122d466d383d19c991fcc2358ea77
River <riverfor@gmail.com>
2017-07-27 18:08:27 +0800

    Up

--- course/build.sh
+++ course/build.sh
@@ -7,4 +7,4 @@ make html
 
 cd build/html && find . -name "*.html" -exec python ../../analytics.py {} \; && python ../../replace.py && find . -name "*-e" -exec rm {} \;&& mv _static static && [ -d _images ] &&  mv _images  images
 
-cd ../.. && mv build/html  ../docs && cp CNAME ../docs  && cp -r quick-start ../docs && cp favicon.ico  ../docs && cp index.html ../docs
+cd ../.. && mv build/html  ../docs && cp CNAME ../docs  && cp favicon.ico  ../docs && cp index.html ../docs
