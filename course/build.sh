5febe09394841818345a6479e3e819152d8b94b8
River <riverfor@gmail.com>
2017-11-15 10:09:51 +0800

    Update source

--- course/build.sh
+++ course/build.sh
@@ -7,4 +7,4 @@ make html
 
 cd build/html && find . -name "*.html" -exec python ../../analytics.py {} \; && find . -name "*.html" -exec sed -i -e 's/_static/static/g;s/_images/images/g' {} \; && python ../../replace.py && find . -name "*-e" -exec rm {} \;&& mv _static static && [ -d _images ] &&  mv _images  images
 
-cd ../.. && mv build/html  ../docs && cp -r index ../docs && cp CNAME ../docs  && cp favicon.ico  ../docs && cp index.html ../docs
+cd ../.. && mv build/html  ../docs && cp -r index ../docs && cp CNAME ../docs  && cp favicon.ico  ../docs && cp index.html ../docs && ls ../docs && mv ../docs/_sources ../docs/sources
